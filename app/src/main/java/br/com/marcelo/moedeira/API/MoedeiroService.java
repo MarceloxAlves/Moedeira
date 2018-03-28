b3b2ed513c32d680cebc0753edea73037bf0db6c
Marcelo Alves <marceloalvessoft@gmail.com>
2018-03-28 01:25:52 -0300

    ainda n compleo

--- app/src/main/java/br/com/marcelo/moedeira/API/MoedeiroService.java
+++ app/src/main/java/br/com/marcelo/moedeira/API/MoedeiroService.java
@@ -1,6 +1,10 @@
 package br.com.marcelo.moedeira.API;
 
+import java.util.List;
+
 import br.com.marcelo.moedeira.model.Moeda;
+import br.com.marcelo.moedeira.model.Noticia;
+import br.com.marcelo.moedeira.model.Service;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
@@ -9,4 +13,11 @@ public interface MoedeiroService {
 
     @GET("rest.php?class=JogosArcadeService&method=subtotal")
     Call<Moeda> listMoeda();
+
+
+
+    /*** NOTICIA ***/
+
+    @GET("rest.php?class=NoticiaService&method=getNoticias")
+    Call<Service> getNoticias();
 }
