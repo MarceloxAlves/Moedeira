d5ed96d5feb5125b0baa0002ae6753abf9289163
jesusadadfilho <jesusadadfilho@gmail.com>
2018-04-06 17:01:14 -0300

    item noticia

---
+++ app/src/main/java/br/com/marcelo/moedeira/app/ItemNoticiaViewActivity.java
@@ -0,0 +1,16 @@
+package br.com.marcelo.moedeira.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.marcelo.moedeira.R;
+
+public class ItemNoticiaViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_noticia_view);
+        //TODO :: instanciar as coisas
+    }
+}
